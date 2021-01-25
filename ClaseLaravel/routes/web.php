212e7996f68665be3394f525ad04e1d567514a30
IvanCastilloE <42181297+IvanCastilloE@users.noreply.github.com>
2021-01-25 14:22:29 -0700

    Initial commit

---
+++ ClaseLaravel/routes/web.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+/*
+Route::get('/', function () {
+    return 'epik';
+});
+
+Route::get('/', function () {
+    return [
+        'clases' => [
+            'Instalacion Laravel',
+            'Routes laravel',
+            'views laravel'
+        ]
+        ];
+});
+Route::get('clases', function() {
+    return 'Aqui se visualisan todas las clases';
+});
+
+Route::get('clases/temas', function() {
+    return 'Aqui se visualisan todos los temas de clase';
+});
+*/
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('notas', function() {
+    return 'Aqui se visualisan todas las notas';
+});
+
+Route::get('notas/{id}/editar', function($id) {
+    return 'Aqui vamos a editar las notas: '.$id;
+});
